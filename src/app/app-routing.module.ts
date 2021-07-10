ccf51403f36eef440b4b42d2f26274b0bc32cd81
mauro-titanio <maurotitanio@gmail.com>
2021-07-10 21:47:00 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { VerificationComponent } from './components/verification/verification.co
 import { AuthGuard } from './shared/services/auth/guard/guard.guard';
 
 const routes: Routes = [
-  { path: '', component: SignInComponent, pathMatch: 'full' },
+  { path: '', component: SignInComponent, pathMatch: 'full'},
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'verification', component: VerificationComponent, canActivate: [AuthGuard] },
   { path: 'profile/:userID/:assID', component: AssociationProfileComponent },
