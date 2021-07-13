33567f565afae7dbed9285018528d44f05fe674b
mauro-titanio <maurotitanio@gmail.com>
2021-07-13 16:11:00 +0200

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  host: {'class': 'min-vh-100'}
 })
 export class AppComponent {
   title = 'plataforma-asociaciones-clm';
